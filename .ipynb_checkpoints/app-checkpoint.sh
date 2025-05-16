875d259f41d8679b792dfbcf958c1fe3f7891e4d
Nick Goble <nick.goble@dominodatalab.com>
2025-05-16 13:54:09 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/nick_goble/fsi-market-data?executionId=68279e72abff3841a2719117

--- .ipynb_checkpoints/app-checkpoint.sh
+++ .ipynb_checkpoints/app-checkpoint.sh
@@ -1,11 +1,14 @@
 mkdir ~/.streamlit
-echo "[browser]" > ~/.streamlit/config.toml
-echo "gatherUsageStats = true" >> ~/.streamlit/config.toml
-echo "serverAddress = "0.0.0.0"" >> ~/.streamlit/config.toml
-echo "serverPort = 8888" >> ~/.streamlit/config.toml
-echo "[server]" >> ~/.streamlit/config.toml
-echo "port = 8888" >> ~/.streamlit/config.toml
-echo "enableCORS = false" >> ~/.streamlit/config.toml
-echo "enableXsrfProtection = false" >> ~/.streamlit/config.toml
+
+cat <<EOF > ~/.streamlit/config.toml
+[browser]
+gatherUsageStats = true
+
+[server]
+address = "0.0.0.0"
+port = 8888
+enableCORS = false
+enableXsrfProtection = false
+EOF
 
 streamlit run dashboard.py
