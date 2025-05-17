1ad5e1134214a8a7e6085dfbbd37213cba966f70
Nick Goble <nick.goble@dominodatalab.com>
2025-05-17 14:40:59 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/nick_goble/fsi-market-data?executionId=68279e72abff3841a2719117

--- .ipynb_checkpoints/app-checkpoint.sh
+++ .ipynb_checkpoints/app-checkpoint.sh
@@ -9,6 +9,14 @@ address = "0.0.0.0"
 port = 8888
 enableCORS = false
 enableXsrfProtection = false
+
+[theme]
+primaryColor = "#543FDE"              # purple500
+backgroundColor = "#FFFFFF"           # neutralLight50
+secondaryBackgroundColor = "#FAFAFA"  # neutralLight100
+textColor = "#2E2E38"                 # neutralDark700
+font = "Inter"                        # explicit Inter
+
 EOF
 
 streamlit run dashboard.py
