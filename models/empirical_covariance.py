7f22bf111e36927bda10dccc34f7b3c346ffe595
Nick Goble <nick.goble@dominodatalab.com>
2025-06-06 16:15:43 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/nick_goble/fsi-market-data?executionId=6843590d9ff323119299fbef

--- models/empirical_covariance.py
+++ models/empirical_covariance.py
@@ -1,35 +1,22 @@
-import mlflow.pyfunc
 import numpy as np
-from mlflow.pyfunc import PythonModel
-from sklearn.covariance import EmpiricalCovariance as _EC
+from sklearn.base import BaseEstimator
+from sklearn.covariance import EmpiricalCovariance
 
-class EmpiricalCovarianceModel(PythonModel):
-    """
-    A combined MLflow pyfunc model that fits an empirical covariance and predicts
-    repeated covariance matrices per input row.
 
-    Usage:
-        model = EmpiricalCovarianceModel().fit(X)
-        mlflow.pyfunc.log_model(
-            artifact_path="emp_cov_model",
-            python_model=model,
-            registered_model_name="EmpiricalCovarianceModel"
-        )
-    """
+class EmpiricalCovarianceModel(BaseEstimator):
     def __init__(self):
-        # PythonModel has its own initializer
-        super().__init__()
         self._cov = None
 
     def fit(self, X, y=None):
-        # Accept DataFrame or numpy array
-        arr = X.values if hasattr(X, "values") else np.asarray(X)
-        ec = _EC().fit(arr)
+        from sklearn.covariance import EmpiricalCovariance as _EC
+        ec = _EC().fit(X)
         self._cov = ec.covariance_
         return self
 
-    def predict(self, context, model_input):
-        # Accept DataFrame or numpy array
-        arr = model_input.values if hasattr(model_input, "values") else np.asarray(model_input)
-        m, n = arr.shape
+    def predict(self, X):
+        m, n = X.shape
         return np.repeat(self._cov[np.newaxis, :, :], repeats=m, axis=0)
+
+    @property
+    def covariance_(self):
+        return self._cov
